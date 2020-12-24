4ea0b7f0c5f7243d132cfcf8ee5ac2b7ad1bea04
thepassle <pascalschilp@gmail.com>
2020-12-24 13:26:51 +0100

    chore: expose devtools and vbump

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-
+export { registerDevtools } from './src/devtools.js';
 export { LitAtom } from './src/lit.js';
 export { atom, selector } from './src/core.js';
---
+++ src/devtools.js
@@ -0,0 +1,53 @@
+import { Atom, Selector } from "./Store.js";
+import { atoms, selectors } from './core.js';
+
+export function registerDevtools() {
+  window.__atoms = atoms;
+  window.__selectors = selectors;
+  
+  const oldSelectorNotify = Selector.prototype.notify;
+  Selector.prototype.notify = function(dispatch = true) {
+    if(dispatch) {
+      document.dispatchEvent(new CustomEvent('__SELECTOR', {
+        detail: {
+          key: this.key,
+          value: this.value
+        }
+      }));
+    }
+    oldSelectorNotify.call(this);
+  };
+
+  const oldAtomNotify = Atom.prototype.notify;
+  Atom.prototype.notify = function(dispatch = true) {
+    if(dispatch) {
+      document.dispatchEvent(new CustomEvent('__ATOM', {
+        detail: {
+          key: this.key,
+          state: this.state
+        }
+      }));
+    }
+    oldAtomNotify.call(this);
+  };
+  
+  requestIdleCallback(() => {
+    const allAtoms = {};
+    atoms.forEach((val, key) => {
+      allAtoms[key] = val.state;
+    });
+  
+    document.dispatchEvent(new CustomEvent('__ATOM_INIT', {
+      detail: allAtoms
+    }));
+
+    const allSelectors = {};
+    selectors.forEach((val, key) => {
+      allSelectors[key] = val.value;
+    });
+  
+    document.dispatchEvent(new CustomEvent('__SELECTOR_INIT', {
+      detail: allSelectors
+    }));
+  });
+}
