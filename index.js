cefab31a45f095d13bfe98ff41c91e009580bdff
James Garbutt <43081j@users.noreply.github.com>
2020-12-26 17:46:38 +0000

    add to index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { registerDevtools } from './src/devtools.js';
 export { LitAtom } from './integrations/lit.js';
+export { AtomLitElement } from './integrations/lit-auto.js';
 export { atom, selector } from './src/core.js';
